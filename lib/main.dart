4fae4d91cadcedbb20bf7ab34f13ea65c182884b
Junior Lopes <juniorlopes@Juniors-MacBook-Pro.local>
2024-10-01 21:43:51 -0300

    update readme

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,6 @@ import 'package:todo_list/app/app_widget.dart';
 
 import 'core/scripts.dart';
 
-///TODO:
-///* Auth and Firestore SDK
-///* Api Rest
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
